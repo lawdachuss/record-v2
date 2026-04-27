17b1f7c7b0972c251a8b7b18c542cc82045625b4
vasud3v <vasud3v@users.noreply.github.com>
2026-04-27 18:25:45 +0530

    Change segment duration to 1 hour

--- github_actions/github_actions_mode.go
+++ github_actions/github_actions_mode.go
@@ -566,7 +566,7 @@ func (gam *GitHubActionsMode) CreateChannelConfigWithQuality(username, site stri
 		Framerate:   30,  // Default framerate (will be overridden by quality selector)
 		Resolution:  1080, // Default resolution (will be overridden by quality selector)
 		Pattern:     "videos/{{if ne .Site \"chaturbate\"}}{{.Site}}/{{end}}{{.Username}}_{{.Year}}-{{.Month}}-{{.Day}}_{{.Hour}}-{{.Minute}}-{{.Second}}{{if .Sequence}}_{{.Sequence}}{{end}}",
-		MaxDuration: 30, // Split into 30-minute segments to enable background uploads
+		MaxDuration: 60, // Split into 1-hour segments to enable background uploads
 		MaxFilesize: 0,  // No filesize limit by default
 	}
 	
